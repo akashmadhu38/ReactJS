9509d1e71a759f00276ab3255af29937aa81ff5b
akashmadhu38 <76092167+akashmadhu38@users.noreply.github.com>
2021-06-05 23:39:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript & ReactJS</h1>
+      <p>
+        Hi sir, this is Akash Madhu from Sree Narayana Gurukulam College Of
+        Engineering, Ernakulam. I have attended your on ReactJS. Thank you.
+      </p>
+    </div>
+  );
+}
+export default Note;
